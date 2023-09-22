1641b94eacad64e4ae263755d0a1959885f92dc3
A22NicolasMR <a22nicolasmr@a21eql18.sanclemente.local>
2023-09-22 14:04:27 +0200

    añadidas un monton de cosas a la aplicacion

--- UD01_1Proyecto/app/src/main/java/com/example/ud01_1proyecto/MainActivity.kt
+++ UD01_1Proyecto/app/src/main/java/com/example/ud01_1proyecto/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.ud01_1proyecto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        val buttonSearch = findViewById<Button>(R.id.button)
+        buttonSearch.setOnClickListener {
+            val spinnerWineTypes= findViewById<Spinner>(R.id.wineType)
+            val textTypesWines =findViewById<TextView>(R.id.textTypesWines)
+            textTypesWines.text=getWines(spinnerWineTypes.selectedItemId).joinToString(",")
+
+            textTypesWines.text ="color seleccionado= ${spinnerWineTypes.selectedItem}"
+        }
+
+
+        }
+    fun getWines(id:Long):List<String> = when(id){
+        0L->listOf("Albariño","Moscato","Rosado")
+        1L->listOf("Rioja","Ribera del Duero","Toro")
+        2L->listOf("Cabernet","Franc","Merlot")
+        else ->
+            listOf()
+        }
 }
