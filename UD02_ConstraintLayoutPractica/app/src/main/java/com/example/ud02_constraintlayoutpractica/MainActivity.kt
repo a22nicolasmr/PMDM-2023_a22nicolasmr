ec93c3e4734516ad5d1a847157ee4b7393a3623e
A22NicolasMR <a22nicolasmr@a21eql18.sanclemente.local>
2023-10-13 14:02:37 +0200

    hecha constraint layout practica SIN MULTILINE

--- UD02_ConstraintLayoutPractica/app/src/main/java/com/example/ud02_constraintlayoutpractica/MainActivity.kt
+++ UD02_ConstraintLayoutPractica/app/src/main/java/com/example/ud02_constraintlayoutpractica/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.ud02_constraintlayoutpractica
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var boton= findViewById<Button>(R.id.button)
+        var textOculto= findViewById<TextView>(R.id.aparece)
+
+        var usuario= findViewById<TextView>(R.id.textView1)
+        var contraseña= findViewById<TextView>(R.id.textView2)
+
+        boton.setOnClickListener{
+            textOculto.setText("Usuario "+ usuario.text +"Contraseña "+contraseña.text)
+        }
     }
 }
