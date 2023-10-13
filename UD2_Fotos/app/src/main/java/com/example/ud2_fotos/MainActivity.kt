67c0aab9741a3fa7d707617fb908ffebaf78f79b
A22NicolasMR <a22nicolasmr@a21eql18.sanclemente.local>
2023-10-13 12:38:55 +0200

    añadidos todos los proyectos

---
+++ UD2_Fotos/app/src/main/java/com/example/ud2_fotos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ud2_fotos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
